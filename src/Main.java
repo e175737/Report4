2dfdb0c4b175e9d2ca70aabbe1d2af395a416be7
Ayaka Toma <e175737@ie.u-ryukyu.ac.jp>
2020-02-03 21:52:53 +0900

    ステップ３

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        try{
-            String str = null;
-            int len = str.length();
-            System.out.println(len);
-        }catch (NullPointerException e){
-            System.out.println("NullPointerException!!!");
-            System.out.println(e.getMessage());
-        }
-
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
 }
